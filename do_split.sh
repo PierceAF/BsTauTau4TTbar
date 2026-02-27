5e1f983b556d3f8e1ae89f44c612d45caa7cc651
ytakahas <Yuta.Takahashi@cern.ch>
2026-02-27 14:54:11 +0100

    update

--- do_split.sh
+++ do_split.sh
@@ -1,4 +1,1 @@
-#python3 split.py -i /eos/cms/store/group/phys_bphys/ytakahas/bstautau/signal_Feb24/260223_201832_0000_mu -o root/signal
-python3 split.py -i /eos/cms/store/group/phys_bphys/ytakahas/bstautau/skimmed/tt_semileptonic/260224_160632__mu -o root/ttbar
-
-#/eos/cms/store/group/phys_bphys/ytakahas/bstautau/flat/tt_semileptonic/
+python3 split.py -i /eos/cms/store/group/phys_bphys/ytakahas/bstautau/skimmed/tt_semileptonic/260224_160632__mu -o /eos/cms/store/group/phys_bphys/ytakahas/bstautau/flat/tt_semileptonic/
